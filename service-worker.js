51a30dcadc2bf7f047ad3178d114bb67b1a41fe4
manuelavellanedazamorano-glitch <manuel.avellaneda.zamorano@gmail.com>
2026-05-15 04:42:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='cti-ia-dss-v6-historico';
+const CACHE='cti-v9-ahp-protegido';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./app_data.js','./logo.png','./icon-192.png','./icon-512.png','./manifest.webmanifest','./CTI_IA_DSS_100_CASOS_RESUELTOS.json','./CTI_IA_DSS_100_HISTORICO_VALIDADO.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
