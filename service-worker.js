5d8872621715c16f3a28595467538d36a89229db
manuelavellanedazamorano-glitch <manuel.avellaneda.zamorano@gmail.com>
2026-05-17 22:53:53 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='cti-v9-ahp-protegido';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./app_data.js','./logo.png','./icon-192.png','./icon-512.png','./manifest.webmanifest','./CTI_IA_DSS_100_CASOS_RESUELTOS.json','./CTI_IA_DSS_100_HISTORICO_VALIDADO.json'];
+const CACHE='cti-v14-abcd-safe';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./app_data.js','./logo.png','./icon-192.png','./icon-512.png','./manifest.webmanifest','./CTI_IA_DSS_100_CASOS_RESUELTOS.json','./CTI_IA_DSS_100_HISTORICO_VALIDADO.json','./analisis_metodologico_abcd.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
