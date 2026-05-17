2de9c2f84de2baab5fc191776b5602db66b57eb6
manuelavellanedazamorano-glitch <manuel.avellaneda.zamorano@gmail.com>
2026-05-17 22:30:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,5 @@
-const CACHE_NAME = 'cti-ia-dss-v12-reparacion-funcional';
-const ASSETS = [
-  './',
-  'index.html',
-  'styles.css',
-  'app.js',
-  'app_data.js',
-  'manifest.webmanifest',
-  'logo.png',
-  'icon-192.png',
-  'icon-512.png',
-  'CTI_IA_DSS_100_CASOS_RESUELTOS.json',
-  'CTI_IA_DSS_100_HISTORICO_VALIDADO.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {}));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
+const CACHE='cti-v9-ahp-protegido';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./app_data.js','./logo.png','./icon-192.png','./icon-512.png','./manifest.webmanifest','./CTI_IA_DSS_100_CASOS_RESUELTOS.json','./CTI_IA_DSS_100_HISTORICO_VALIDADO.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
